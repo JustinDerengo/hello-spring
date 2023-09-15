ced3a99cd876763d7cfbfce77e4c46399e49ee6d
Allegorylla <138933736+JustinDerengo@users.noreply.github.com>
2023-09-15 14:30:26 -0600

    Thymeleaf lesson

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Controller
 public class HelloController {
@@ -43,6 +46,16 @@ public class HelloController {
     @GetMapping("form")
     public String helloForm() {
         return "form";
+    }
+
+    @GetMapping("hello-names")
+    public String helloNames(Model model) {
+        List<String> names = new ArrayList<>();
+        names.add("Bob");
+        names.add("Jim");
+        names.add("Carol");
+        model.addAttribute("names", names);
+        return "hello-list";
     }
 
 }
